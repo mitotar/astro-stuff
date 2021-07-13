95afc789894e706193403edb8386fc98b48dbfaf
Michael <michael.totarella@mail.concordia.ca>
2021-07-12 20:56:15 -0400

    Created luminosity function and unit tests

--- src/luminosity_brightness.py
+++ src/luminosity_brightness.py
@@ -1,1 +1,10 @@
 import astropy.constants as const
+
+
+def stefan_boltzmann(area, temp):
+    """
+    Calculate the luminosity (W) of a spherical blackbody with the given surface area (m^2) and temperature (K) using the Stefan_boltzmann law.
+    """
+    if area < 0 or temp < 0:
+        raise ValueError("Area and temperature values must be non-negative.")
+    return const.sigma_sb.value * area * temp ** 4
--- tests/test_luminosity_brightness.py
+++ tests/test_luminosity_brightness.py
@@ -1,8 +1,45 @@
 import pytest
 import astropy.constants as const
-import src.luminosity_brightness
+import src.luminosity_brightness as lb
 
 
 class TestStefanBoltzmann(object):
-    def test_positive_temp(self):
-        pass
+    def test_negative_area_negative_temp(self):
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(-10, -2000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(-1e30, -5000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+
+    def test_negative_area_positive_temp(self):
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(-10, 2000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(-1e30, 5000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+
+    def test_positive_area_negative_temp(self):
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(10, -2000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+        with pytest.raises(ValueError) as e:
+            calc = lb.stefan_boltzmann(1e30, -5000)
+        assert e.match(
+            "Area and temperature values must be non-negative."), "Expected ValueError, but got {0}.".format(calc)
+
+    def test_positive_area_positive_temp(self):
+        theory = 0.00567
+        calc = lb.stefan_boltzmann(10, 10)
+        assert calc == pytest.approx(
+            theory, 1e-4), "Expected {0}, but got {1}.".format(theory, calc)
+        theory = 5.67e20
+        calc = lb.stefan_boltzmann(1e20, 1000)
+        assert calc == pytest.approx(
+            theory, 1e18), "Expected {0}, but got {1}.".format(theory, calc)
